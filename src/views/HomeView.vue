116fbb2b9de850f9607e9228fd1e5ad969bfccbb
Mustafa Kemal Karadeniz <108372741+M-kemal@users.noreply.github.com>
2023-07-21 17:41:09 +0300

    veriler dinamik alındı

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,101 +1,164 @@
 <template>
-  <div class="home flex flex-col items-center h-screen justify-center w-full mx-auto bg-gray-100">
-
-    <div class="max-w-5xl ">
+  <div
+    class="home flex flex-col items-center h-screen justify-center w-full mx-auto bg-gray-100"
+  >
+    <div v-if="!testCompleted" class="max-w-5xl">
       <div class="topbar flex items-center w-full mb-4">
-
         <div class="w-[40%] sm:w-[20%]">
-
-          <button class="sm:px-6 px-4 py-1 text-slate-500 shadow-md bg-slate-50 border-slate-400 rounded-l-full sm:mr-2 hover:bg-white hover:text-black hover:border-slate-500 duration-300 ease-linear">
+          <button
+            class="sm:px-6 px-4 py-1 text-slate-500 shadow-md bg-slate-50 border-slate-400 rounded-l-full sm:mr-2 hover:bg-white hover:text-black hover:border-slate-500 duration-300 ease-linear"
+            @click="goBack"
+          >
             Go Back
           </button>
-
         </div>
-        <div class="progresbar relative bg-slate-50 border border-slate-300 shadow-md w-[99%] sm:w-full h-8 rounded-r-full">
-
-          <div class="bg-cyan-300 flex items-center h-8 rounded-r-sm duration-300 ease-linear">
-
+        <div
+          class="progresbar relative bg-slate-50 border border-slate-300 shadow-md w-[99%] sm:w-full h-8 rounded-r-full"
+        >
+          <div
+            class="bg-cyan-300 flex items-center h-8 rounded-r-sm duration-300 ease-linear"
+            :style="{ width: progresBarWidth }"
+          >
             <div class="absolute top-1 left-2 sm:left-1/2 sm:-translate-x-1/2">
-              <div class="flex space-x-2 ">
-
+              <div class="flex space-x-2">
                 <p class="font-light">
                   ITEM
 
-                  <span class="font-bold">1</span>
-
+                  <span class="font-bold">{{ currentItem + 1 }}</span>
                 </p>
 
-                <p class="font-light">
-                  FROM 36
-                </p>
+                <p class="font-light">FROM 36</p>
 
                 <p class="">
-                  (<span class="font-bold">0%</span>)
-
+                  (<span class="font-bold"> {{ progressPercentage }}%</span>)
                 </p>
-
               </div>
-
             </div>
-
           </div>
-
         </div>
-
       </div>
 
       <div class="questionImg w-full shadow-lg">
-
-        <img src="/images/0.jpg" class="object-cover mx-auto rounded" alt="">
-
+        <img
+          :src="currentQuestion.image"
+          class="object-cover mx-auto rounded"
+          alt=""
+        />
       </div>
 
       <div class="answerButton flex flex-col w-full mx-auto mt-4">
+        <button
+          class="px-8 py-4 bg-slate-50 border border-slate-300 rounded-xl shadow-md hover:bg-white hover:shadow-lg hover:border-b-slate-400 mb-3 duration-300 ease-linear capitalize"
+          @click="nextQuestion"
+          v-for="(answer, index) in currentQuestion.answers"
+          :key="index"
+        >
+          {{ answer }}
+        </button>
 
-        <button class="px-8 py-4 bg-slate-50 border border-slate-300 rounded-xl shadow-md hover:bg-white hover:shadow-lg hover:border-b-slate-400 mb-3 duration-300 ease-linear capitalize">
+        <!-- <button
+          class="px-8 py-4 bg-slate-50 border border-slate-300 rounded-xl shadow-md hover:bg-white hover:shadow-lg hover:border-b-slate-400 mb-3 duration-300 ease-linear capitalize"
+        >
           jealous
         </button>
 
-        <button class="px-8 py-4 bg-slate-50 border border-slate-300 rounded-xl shadow-md hover:bg-white hover:shadow-lg hover:border-b-slate-400 mb-3 duration-300 ease-linear capitalize">
+        <button
+          class="px-8 py-4 bg-slate-50 border border-slate-300 rounded-xl shadow-md hover:bg-white hover:shadow-lg hover:border-b-slate-400 mb-3 duration-300 ease-linear capitalize"
+        >
           jealous
         </button>
 
-        <button class="px-8 py-4 bg-slate-50 border border-slate-300 rounded-xl shadow-md hover:bg-white hover:shadow-lg hover:border-b-slate-400 mb-3 duration-300 ease-linear capitalize">
+        <button
+          class="px-8 py-4 bg-slate-50 border border-slate-300 rounded-xl shadow-md hover:bg-white hover:shadow-lg hover:border-b-slate-400 mb-3 duration-300 ease-linear capitalize"
+        >
           jealous
-        </button>
-
-        <button class="px-8 py-4 bg-slate-50 border border-slate-300 rounded-xl shadow-md hover:bg-white hover:shadow-lg hover:border-b-slate-400 mb-3 duration-300 ease-linear capitalize">
-          jealous
-        </button>
-
+        </button> -->
       </div>
-
     </div>
 
-    <div class="">
+    <div v-else class="">
       <div class="flex flex-col items-center mt-4">
+        <p class="">Testi tekrarlamak istermisiniz?</p>
 
-        <p class="">
-          Testi tekrarlamak istermisiniz?
-        </p>
-
-        <button class="px-6 py-4 bg-cyan-300 mt-2 ">
+        <button @click="resetBtn" class="px-6 py-4 bg-cyan-300 mt-2">
           EVET
         </button>
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import dataQuestions from "@/data.json";
+import { reactive, ref } from "vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    
-  }
-}
+  name: "HomeView",
+  components: {},
+  setup() {
+    const questions = reactive(dataQuestions);
+
+    const totalQuestion = questions.length;
+
+    const currentItem = ref(0);
+
+    const progresBarWidth = ref("0%");
+
+    const progressPercentage = ref(0);
+
+    const testCompleted = ref(false);
+
+    const currentQuestion = ref(questions[currentItem.value]);
+
+    const nextQuestion = () => {
+      currentItem.value++;
+
+      const selectedAnswerPercentage =
+        (currentItem.value / totalQuestion) * 100;
+      progresBarWidth.value = `${selectedAnswerPercentage}%`;
+      progressPercentage.value = Math.floor(
+        (currentItem.value / totalQuestion) * 100
+      );
+
+      if (currentItem.value === totalQuestion) {
+        testCompleted.value = true;
+        return;
+      }
+
+      currentQuestion.value = questions[currentItem.value];
+    };
+
+    const goBack = () => {
+      if (currentItem.value > 0) {
+        currentItem.value--;
+        const selectedAnswerPercentage =
+          (currentItem.value / totalQuestion) * 100;
+        progresBarWidth.value = `${selectedAnswerPercentage}%`;
+        progressPercentage.value = Math.floor(
+          (currentItem.value / totalQuestion) * 100
+        );
+      }
+      currentQuestion.value = questions[currentItem.value];
+    };
+
+    const resetBtn = () => {
+      currentItem.value = 0;
+      progresBarWidth.value = "0%";
+      progressPercentage.value = 0;
+      currentQuestion.value = questions[currentItem.value];
+      testCompleted.value = false;
+    };
+
+    return {
+      currentItem,
+      progresBarWidth,
+      progressPercentage,
+      currentQuestion,
+      nextQuestion,
+      goBack,
+      resetBtn,
+      testCompleted,
+    };
+  },
+};
 </script>
