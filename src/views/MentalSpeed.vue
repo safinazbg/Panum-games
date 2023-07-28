567ab688c2f4ddd9166a74d2060f698403273423
Mustafa Kemal Karadeniz <108372741+M-kemal@users.noreply.github.com>
2023-07-28 14:17:09 +0300

    update

---
+++ src/views/MentalSpeed.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <arrowTop></arrowTop>
+  </div>
+</template>
+
+<script>
+import arrowTop from "@/components/Icons/arrowTop.vue";
+
+export default {
+  components: { arrowTop },
+};
+</script>
