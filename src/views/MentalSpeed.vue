170e4cb84dc1472ea6da6f4f84e95ce610fc1b6f
Yasin BUYUKGUZEL <116117219+YasinBG@users.noreply.github.com>
2023-07-28 14:43:28 +0300

    mental speed start

--- src/views/MentalSpeed.vue
+++ src/views/MentalSpeed.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <arrowTop></arrowTop>
+ <div class="container mx-auto w-full  flex border-b-2 border-black">
+  <div class="w-4/5 h-16 ml-44">
+    <p class="w-1/2 text-2xl font-bold">
+        Panum
+    </p>
+    <p class="text-sm text-gray-500 font-n">
+      Designed to train your brain
+    </p>
+    
   </div>
+  <div class="w-1/5 h-16 border-l-2 border-black flex justify-center items-center">
+      <button class="border-2 border-black rounded-full text-center w-36 h-8 flex justify-center items-center">Log in
+        <span class="ml-3"><arrowTop class="h-4 w-4"></arrowTop></span>
+      </button>
+  </div>
+
+ </div>
+    
+  
 </template>
 
 <script>
