3d0015a2f247586597be88c2a1adbb0612eeb867
Mustafa Kemal Karadeniz <108372741+M-kemal@users.noreply.github.com>
2023-08-10 15:40:04 +0300

    mental test düzenleme

--- src/views/MentalTest.vue
+++ src/views/MentalTest.vue
@@ -6,8 +6,20 @@
     class="WelcomePage container mx-auto flex flex-col items-center h-screen fadeInAnimation"
   >
     <div class="title flex flex-col justify-evenly items-center h-full py-20">
-      <h1 class="text-[min(10vw,3.75rem)] text-center">Mental Speed Test</h1>
-      <p>Some welcome message/explenation</p>
+      <h1 class="text-[min(10vw,3.75rem)] text-center">
+        Welcome to the Mental speed task!
+      </h1>
+      <p class="text-center w-1/2 text-lg">
+        Instructions: <br />
+        You will be shown stimuli on the screen along with 2 choices. Your task
+        is to indicate which of the 2 choices best matches the stimuli, and do
+        it as fast as possible, using the left and right arrow keys.
+      </p>
+      <p class="text-lg">
+        You will start with level 1 and then proceed to level 2.
+      </p>
+      <p class="text-lg">The 2 first rounds are always trials.</p>
+      <p class="text-lg">Are you ready?</p>
       <div class="flex flex-col">
         <p class="text-center mb-14">
           Press left or right arrow key to continue
@@ -21,6 +33,83 @@
           </button>
           <button
             @click="startTest"
+            class="bg-red-500 px-16 py-4 border-4 border-black rounded-xl hover:bg-red-600 duration-150 ease-linear"
+          >
+            <chevronIcon class="w-8" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- level 1 -->
+  <div
+    v-if="page2"
+    class="WelcomePage container mx-auto flex flex-col items-center h-screen fadeInAnimation"
+  >
+    <div
+      class="title flex flex-col justify-evenly items-center h-full py-20 text-center"
+    >
+      <h1 class="text-[min(10vw,3.75rem)] text-center">
+        Level 1 - Search task
+      </h1>
+      <p class="w-1/2 text-lg">
+        Now you will be shown a single stimulus on the screen, which are either
+        numbers, letters or symbols. You just have to choose which given choice
+        best matches them. Remember to be fast!
+      </p>
+      <p class="text-lg">The first 2 rounds are trials.</p>
+      <p class="text-lg">Good luck and have fun!</p>
+      <div class="flex flex-col">
+        <p class="text-center mb-14">
+          Press left or right arrow key to continue
+        </p>
+        <div class="flex space-x-8">
+          <button
+            @click="Level1Start"
+            class="bg-green-500 px-16 py-4 border-4 border-black rounded-xl hover:bg-green-400 duration-150 ease-linear"
+          >
+            <chevronIcon class="w-8 rotate-180" />
+          </button>
+          <button
+            @click="Level1Start"
+            class="bg-red-500 px-16 py-4 border-4 border-black rounded-xl hover:bg-red-600 duration-150 ease-linear"
+          >
+            <chevronIcon class="w-8" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- level 2 -->
+  <div
+    v-if="level2"
+    class="WelcomePage container mx-auto flex flex-col items-center h-screen fadeInAnimation"
+  >
+    <div class="title flex flex-col justify-evenly items-center h-full py-20">
+      <h1 class="text-[min(10vw,3.75rem)] text-center">
+        Level 2 - Comparison task
+      </h1>
+      <p class="text-center w-1/2 text-lg">
+        Now you will be shown a pair of 3 stimuli on the screen, which are again
+        either numbers, letters or symbols. You just have to indicate whether
+        they are the same or different from each other. Remember to be fast!
+      </p>
+      <p class="text-lg">The first 2 rounds are trials again.</p>
+      <p class="text-lg">TGood luck and have fun!</p>
+      <div class="flex flex-col">
+        <p class="text-center mb-14">
+          Press left or right arrow key to continue
+        </p>
+        <div class="flex space-x-8">
+          <button
+            @click="Level1Start"
+            class="bg-green-500 px-16 py-4 border-4 border-black rounded-xl hover:bg-green-400 duration-150 ease-linear"
+          >
+            <chevronIcon class="w-8 rotate-180" />
+          </button>
+          <button
+            @click="Level1Start"
             class="bg-red-500 px-16 py-4 border-4 border-black rounded-xl hover:bg-red-600 duration-150 ease-linear"
           >
             <chevronIcon class="w-8" />
@@ -73,10 +162,10 @@
   >
     <div class="title flex flex-col justify-evenly items-center h-full py-20">
       <h1 class="text-[min(10vw,3.75rem)] text-center flex">
-        Tank you for playing !
+        Thank you for playing!
       </h1>
-      <p>You can now continue the comparison task</p>
-      <div class="flex flex-col">
+      <p class="text-3xl">You have completed the game. Well done!</p>
+      <!-- <div class="flex flex-col">
         <p class="text-center mb-14 animate-pulse">
           Press left or right arrow key to continue
         </p>
@@ -94,7 +183,7 @@
             <chevronIcon class="w-8" />
           </button>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -113,6 +202,8 @@ export default {
     const isWelcome = ref(true);
     const isGameStart = ref(false);
     const isGameComplated = ref(false);
+    const page2 = ref(false);
+    const level2 = ref(false);
     const task = reactive(mentalSpeed);
     const taskLength = task.length;
     const currentTask = ref(0);
@@ -120,27 +211,49 @@ export default {
 
     const startTest = () => {
       isWelcome.value = false;
-      isGameStart.value = true;
+      page2.value = true;
+      // isGameStart.value = true;
     };
 
+    const Level1Start = () => {
+      page2.value = false;
+      level2.value = false;
+      isGameStart.value = true;
+    };
+    // const nextTask = () => {
+    //   if (currentTask.value < taskLength - 1) {
+    //     currentTask.value++;
+    //   } else {
+    //     isGameStart.value = false;
+    //     isGameComplated.value = true;
+    //   }
+
+    //   currentTaskQuestion.value = task[currentTask.value];
+    // };
     const nextTask = () => {
       if (currentTask.value < taskLength - 1) {
         currentTask.value++;
-      } else {
+        if (currentTask.value >= 10) {
+          level2.value = true;
+          if (currentTask.value >= 11) {
+            level2.value = false;
+          }
+          currentTaskQuestion.value = task[currentTask.value + 1];
+        }
+      } else if (currentTask.value >= 11) {
         isGameStart.value = false;
         isGameComplated.value = true;
       }
-
       currentTaskQuestion.value = task[currentTask.value];
     };
 
-    const resetTest = () => {
-      currentTask.value = 0;
-      currentTaskQuestion.value = task[currentTask.value];
-      isGameComplated.value = false;
-      isWelcome.value = true;
-      isGameStart.value = false;
-    };
+    // const resetTest = () => {
+    //   currentTask.value = 0;
+    //   currentTaskQuestion.value = task[currentTask.value];
+    //   isGameComplated.value = false;
+    //   isWelcome.value = true;
+    //   isGameStart.value = false;
+    // };
 
     watchEffect(() => {
       if (isGameComplated.value) {
@@ -153,9 +266,12 @@ export default {
       isWelcome,
       startTest,
       nextTask,
-      resetTest,
+      // resetTest,
       currentTaskQuestion,
       isGameStart,
+      page2,
+      Level1Start,
+      level2,
     };
   },
 };
