873cc33cb9d38a611ca1d58feabcbb827bccbb0c
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2023-09-19 15:15:57 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,8 +19,8 @@
     <div
       class="grid-group md:grid grid-cols-5 border-b-2 md:border-t-2 border-black"
     >
-
       <div class="col1 md:border-r-2 border-black"></div>
+
       <div @click="workingMemory"
         class="col2 border-b-2 md:border-b-0 md:border-r-2 p-8 border-black flex items-end justify-center"
       >
@@ -35,6 +35,7 @@
           </button>
         </div>
       </div>
+
       <div
         class="col3 md:border-r-2 md:border-b-0 p-8 border-black hover:bg-gray-100 flex items-end justify-center border-b-2"
        >
@@ -51,6 +52,7 @@
         </div>
       </RouterLink>
     </div>
+
       <div
         @click="attention"
         class="col4 md:border-r-2 p-8 border-black flex items-end justify-center cursor-pointer hover:bg-gray-100"
@@ -71,13 +73,16 @@
       </div>
       <div class="col5 border-black"></div>
     </div>
+
     <div class="grid-group md:grid grid-cols-5 border-b-2 border-black">
       <div class="col1 border-r-2 border-black"></div>
+
       <div
-        class="col2 md:border-r-2 md:border-b-0 border-b-2 p-8 border-black flex items-end justify-center"
+        @click="speak"
+          class="col2 md:border-r-2 md:border-b-0 border-b-2 p-8 border-black flex items-end justify-center"
       >
         <div class="icon flex flex-col items-center space-y-5">
-          <p class="font-semibold">Game 4</p>
+          <p class="font-semibold">Speech Recognition</p>
           <button
             class="border-2 border-black rounded-full text-center w-36 h-8 flex justify-center items-center"
           >
@@ -152,7 +157,11 @@ export default {
       router.push("/WorkingMemory");
     };
 
-    return { router, attention, workingMemory };
+    const speak = () => {
+      router.push("/SpeechRecognition");
+    };
+
+    return { router, attention, speak, workingMemory };
   },
 };
 </script>
