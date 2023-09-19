66d68cd54c11dc0527eb752ad45a803c6f357188
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2023-09-19 14:24:57 +0200

    Update SpeechRecognition.vue

--- src/views/SpeechRecognition.vue
+++ src/views/SpeechRecognition.vue
@@ -1,5 +1,9 @@
 <template>
 <div class="speechRecognition">
+  <button @click="ToggleMic">Start Recording</button>
+  <button @click="stopRecording" >Stop Recording</button>
+  <div style="font-size: large" v-text="recognizedText"></div>
+
 
 </div>
 </template>
