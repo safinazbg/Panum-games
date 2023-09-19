52e292894836052e19c7f6dec27786722c9f9b51
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2023-09-19 15:15:59 +0200

    Update SpeechRecognition.vue

--- src/views/SpeechRecognition.vue
+++ src/views/SpeechRecognition.vue
@@ -1,21 +1,32 @@
 <template>
-<div class="speechRecognition">
-  <button @click="ToggleMic">Start Recording</button>
-  <button @click="stopRecording" >Stop Recording</button>
-  <div style="font-size: large" v-text="recognizedText"></div>
+  <div class="speechRecognition">
+    <button @click="ToggleMic"
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"/>
+      </svg>
+      >Start Recording
+    </button>
+    <button @click="stopRecording">Stop Recording</button>
+    <div style="font-size: large" v-text="recognizedText"></div>
 
-  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-    </svg>
-    <span class="sr-only">Icon description</span>
-  </button>
+    <button type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"/>
+      </svg>
+      <span class="sr-only">Icon description</span>
+    </button>
 
-</div>
+  </div>
 </template>
 
 <script>
 import {onMounted, ref} from "vue";
+
 export default {
   name: "SpeechRecognition",
   setup() {
@@ -32,16 +43,16 @@ export default {
 
       sr.onstart = () => {
         console.log('SR started')
-        isRecording.value =true;
+        isRecording.value = true;
       }
       sr.onend = () => {
         console.log('SR stopped')
-        isRecording.value =false;
+        isRecording.value = false;
       }
 
       sr.onresult = (event) => {
         // console.log(event)
-        for (let i = 0; i< event.results.length; i++) {
+        for (let i = 0; i < event.results.length; i++) {
           const result = event.results[i];
 
           if (result.isFinal) {
@@ -65,12 +76,12 @@ export default {
       } else if (
           t.includes('what is the time') ||
           t.includes('what time is it')
-      ){
+      ) {
         sr.stop()
         alert(new Date().toLocaleTimeString())
         setTimeout(() => {
           sr.start()
-        }, 100 )
+        }, 100)
       }
     }
     const ToggleMic = () => {
