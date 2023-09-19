603234db7f48a6d531e8f5dc525083033e76b0a1
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2023-09-19 14:26:27 +0200

    voice recognition

--- src/views/SpeechRecognition.vue
+++ src/views/SpeechRecognition.vue
@@ -15,8 +15,80 @@
 </template>
 
 <script>
+import {onMounted, ref} from "vue";
 export default {
-  name: "SpeechRecognition"
+  name: "SpeechRecognition",
+  setup() {
+    const recognizedText = ref("");
+    const isRecording = ref(false);
+
+    console.log(window.SpeechRecognition)
+    const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const sr = new Recognition();
+
+    onMounted(() => {
+      sr.continuous = true;
+      sr.interimResults = true;
+
+      sr.onstart = () => {
+        console.log('SR started')
+        isRecording.value =true;
+      }
+      sr.onend = () => {
+        console.log('SR stopped')
+        isRecording.value =false;
+      }
+
+      sr.onresult = (event) => {
+        // console.log(event)
+        for (let i = 0; i< event.results.length; i++) {
+          const result = event.results[i];
+
+          if (result.isFinal) {
+            CheckForCommand(result)
+          }
+        }
+
+        const t = Array.from(event.results)
+            .map((result) => result[0])
+            .map((result) => result.transcript)
+            .join('');
+        recognizedText.value = t;
+      }
+
+    })
+
+    const CheckForCommand = (result) => {
+      const t = result[0].transcript;
+      if (t.includes('stop recording')) {
+        sr.stop()
+      } else if (
+          t.includes('what is the time') ||
+          t.includes('what time is it')
+      ){
+        sr.stop()
+        alert(new Date().toLocaleTimeString())
+        setTimeout(() => {
+          sr.start()
+        }, 100 )
+      }
+    }
+    const ToggleMic = () => {
+      if (isRecording.value) {
+        sr.stop();
+      } else {
+        sr.start();
+      }
+    }
+
+
+    return {
+      CheckForCommand,
+      recognizedText,
+      ToggleMic
+
+    }
+  }
 }
 </script>
 
