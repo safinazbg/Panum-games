e6ffd0d2952d0bef06f76e2131fac17f0041f16a
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2023-09-20 10:38:33 +0200

    Update SpeechRecognition.vue

--- src/views/SpeechRecognition.vue
+++ src/views/SpeechRecognition.vue
@@ -1,40 +1,30 @@
 <template>
-  <div class="speechRecognition">
-    <button @click="ToggleMic"
-            type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"/>
-      </svg>
-      >Start Recording
+  <div class="recordMicContainer">
+    <button :class="`mic${isRecording ? ' recording' : ''}`" @click="ToggleMic">
+      {{ isRecording ? "Stop Recording" : "Start Recording" }}
     </button>
-    <button @click="stopRecording">Stop Recording</button>
-    <div style="font-size: large" v-text="recognizedText"></div>
-
-    <button type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"/>
-      </svg>
-      <span class="sr-only">Icon description</span>
-    </button>
-
+    <div class="transcript" v-text="transcript"></div>
+  </div>
+  <div class="sound-wave">
+    <canvas ref="canvas" class="canvas"></canvas>
+    <!-- <button @click="toggleAudioContext">
+      {{ audioContextStarted ? "Record Stop" : "Record Start" }}
+    </button> -->
   </div>
 </template>
 
 <script>
-import {onMounted, ref} from "vue";
-
+import { ref, onMounted } from "vue";
 export default {
   name: "SpeechRecognition",
   setup() {
-    const recognizedText = ref("");
+    const transcript = ref("");
     const isRecording = ref(false);
 
-    console.log(window.SpeechRecognition)
-    const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const canvasRef = ref(null);
+
+    const Recognition =
+        window.SpeechRecognition || window.webkitSpeechRecognition;
     const sr = new Recognition();
 
     onMounted(() => {
@@ -42,67 +32,188 @@ export default {
       sr.interimResults = true;
 
       sr.onstart = () => {
-        console.log('SR started')
+        console.log("SR Started");
         isRecording.value = true;
-      }
+      };
+
       sr.onend = () => {
-        console.log('SR stopped')
+        console.log("SR Stopped");
         isRecording.value = false;
-      }
+      };
 
-      sr.onresult = (event) => {
-        // console.log(event)
-        for (let i = 0; i < event.results.length; i++) {
-          const result = event.results[i];
+      sr.onresult = (evt) => {
+        for (let i = 0; i < evt.results.length; i++) {
+          const result = evt.results[i];
 
-          if (result.isFinal) {
-            CheckForCommand(result)
-          }
+          if (result.isFinal) CheckForCommand(result);
         }
 
-        const t = Array.from(event.results)
+        const t = Array.from(evt.results)
             .map((result) => result[0])
             .map((result) => result.transcript)
-            .join('');
-        recognizedText.value = t;
-      }
+            .join("");
 
-    })
+        transcript.value = t;
+      };
+
+      canvasRef.value = document.querySelector("canvas");
+      if (!canvasRef.value) {
+        console.error("Canvas element not found.");
+      } else {
+        setupCanvas();
+      }
+    });
 
     const CheckForCommand = (result) => {
       const t = result[0].transcript;
-      if (t.includes('stop recording')) {
-        sr.stop()
+      if (t.includes("stop recording")) {
+        sr.stop();
+        if (audioContextStarted.value) {
+          startAudioContext();
+        }
       } else if (
-          t.includes('what is the time') ||
-          t.includes('what time is it')
+          t.includes("what is the time") ||
+          t.includes("what's the time")
       ) {
-        sr.stop()
-        alert(new Date().toLocaleTimeString())
-        setTimeout(() => {
-          sr.start()
-        }, 100)
+        sr.stop();
+        alert(new Date().toLocaleTimeString());
+        setTimeout(() => sr.start(), 100);
       }
-    }
+    };
+
     const ToggleMic = () => {
       if (isRecording.value) {
         sr.stop();
+        if (audioContextStarted.value) {
+          startAudioContext();
+        }
       } else {
+        if (!audioContextStarted.value) {
+          startAudioContext();
+        }
         sr.start();
       }
-    }
+    };
 
+    const audioContext = ref(null);
+    const analyser = ref(null);
+    const canvasContext = ref(null);
+    const canvasWidth = 800;
+    const canvasHeight = 200;
+    const audioContextStarted = ref(false);
+
+    const startAudioContext = async () => {
+      try {
+        if (!audioContextStarted.value) {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+          });
+
+          audioContext.value = new (window.AudioContext ||
+              window.webkitAudioContext)();
+          analyser.value = audioContext.value.createAnalyser();
+          const source = audioContext.value.createMediaStreamSource(stream);
+          source.connect(analyser.value);
+          analyser.value.fftSize = 256;
+
+          audioContextStarted.value = true;
+
+          drawSoundWave();
+        } else {
+          audioContext.value.close();
+          audioContextStarted.value = false;
+          canvasContext.value.clearRect(0, 0, canvasWidth, canvasHeight);
+        }
+      } catch (error) {
+        console.error("Mikrofon erişimi reddedildi:", error);
+      }
+    };
+
+    const setupCanvas = () => {
+      const canvas = canvasRef.value;
+      if (!canvas) {
+        console.error("Canvas element not found.");
+        return;
+      }
+      canvasContext.value = canvas.getContext("2d");
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
+    };
+
+    const drawSoundWave = () => {
+      const dataArray = new Uint8Array(analyser.value.frequencyBinCount);
+      const draw = () => {
+        analyser.value.getByteTimeDomainData(dataArray);
+        canvasContext.value.clearRect(0, 0, canvasWidth, canvasHeight);
+        canvasContext.value.lineWidth = 2;
+        canvasContext.value.strokeStyle = "red";
+        canvasContext.value.beginPath();
+        const sliceWidth = canvasWidth / dataArray.length;
+        let x = 0;
+        for (let i = 0; i < dataArray.length; i++) {
+          const v = dataArray[i] / 128.0;
+          const y = (v * canvasHeight) / 2;
+          if (i === 0) {
+            canvasContext.value.moveTo(x, y);
+          } else {
+            canvasContext.value.lineTo(x, y);
+          }
+          x += sliceWidth;
+        }
+        canvasContext.value.lineTo(canvasWidth, canvasHeight / 2);
+        canvasContext.value.stroke();
+        requestAnimationFrame(draw);
+      };
+      draw();
+    };
 
     return {
-      CheckForCommand,
-      recognizedText,
-      ToggleMic
+      transcript,
+      ToggleMic,
 
-    }
-  }
-}
+      isRecording,
+      canvasRef,
+      audioContextStarted,
+      toggleAudioContext: startAudioContext,
+    };
+  },
+};
 </script>
 
-<style scoped>
+<style lang="css" scoped>
+.recordMicContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+}
 
+.mic {
+  padding: 10px 20px;
+  background-color: #3498db;
+  border: none;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.mic.recording {
+  background-color: #e74c3c;
+}
+
+.transcript {
+  margin-top: 20px;
+  font-size: 18px;
+}
+
+.wave {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+
+.canvas {
+  border: 1px solid #ddd;
+}
 </style>
