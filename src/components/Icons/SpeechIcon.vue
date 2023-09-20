bfcb58cfc42086b194f982f0a38250b3aafd4799
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2023-09-20 21:13:42 +0200

    recording icons

---
+++ src/components/Icons/SpeechIcon.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <rect width="48" height="48" fill="white" fill-opacity="0.01"></rect> <rect x="17" y="4" width="14" height="27" rx="7" fill="#2F88FF" stroke="#000000" stroke-width="4" stroke-linejoin="round"></rect> <path d="M9 23C9 31.2843 15.7157 38 24 38C32.2843 38 39 31.2843 39 23" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M24 38V44" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path> </g>
+  </svg>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "SpeechIcon"
+}
+</script>
+
+<style scoped>
+
+</style>
