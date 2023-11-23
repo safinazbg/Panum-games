c36086c32d56b7019af9b7ca7719b633311abd72
Eske Junker <eskej@live.dk>
2023-11-23 14:34:00 +0100

    working on the progressbar and UI

--- src/components/declarativeKnowledge/DeclarativeKnowledge.vue
+++ src/components/declarativeKnowledge/DeclarativeKnowledge.vue
@@ -1,38 +1,57 @@
 <template>
+  <div class="min-h-screen">
   <PanumNavigation/>
   <div
-      class="flex flex-col items-center justify-center max-w-3xl mx-auto text-center mt-40"
+      class="  max-w-3xl mx-auto text-center mt-24 min-h-[400px] flex flex-col justify-between"
   >
-    <div v-if="!gameStarted">
       <div class="h-20 flex items-center relative w-full justify-between">
-<!--        <div class="bg-gray-500"></div>-->
-      <div class="w-full bg-green-500 h-5 rounded-md absolute" />
-        <div class="z-10" v-for="(style, view) in viewStyles" :key="view">
-          <img :src="style.image" alt="" class="w-12 h-12 z-40 absolute">
-          <div :class="['h-12 w-12 rounded-full', 'bg-' + style.color]">
+<!--        <div class="border-gray-500"></div>-->
+<!--        <div class="border-red-500"></div>-->
+<!--        <div class="border-blue-500"></div>-->
+<!--        <div class="border-yellow-500"></div>-->
+<!--                <div class="bg-gray-500"></div>-->
+<!--                <div class="bg-red-500"></div>-->
+<!--                <div class="bg-blue-500"></div>-->
+<!--                <div class="bg-yellow-500"></div>-->
+<!--                <div class="text-gray-500"></div>-->
+<!--                <div class="text-red-500"></div>-->
+<!--                <div class="text-blue-500"></div>-->
+<!--                <div class="text-yellow-500"></div>-->
+      <div class="border w-[94%] mx-6 bg-slate-200 h-2 rounded-md absolute" >
+        <div class=" w-full shadow-md h-2 bg-slate-700 transition-all duration-500 ease-in-out"
+             :style="{ width: progress && !isNaN(progress) ? progress + '%' : 0 + '%', backgroundColor: progress && !isNaN(progress) ? '#85dcf9' : '' }"></div>
         </div>
+        <div class="  relative " v-for="(style, view) in viewStyles" :key="view">
+          <p class="absolute -top-6 text-gray-600 font-bold left-1/2 -translate-x-1/2 whitespace-nowrap opacity-0 duration-500 transition-opacity" :class="[{'opacity-100': currentView === view}]">
+          {{ view }}
+          </p>
+          <div :class="['h-12 w-12 rounded-full border-4 transition-all duration-500 delay-100 ', { ' border-[#85dcf9] bg-[#85dcf9]': isViewReached(view) }, { '!bg-white': !isViewReached(view) } ]">
+            <component :is="style.image" alt="" class=" z-40 grid  h-full place-items-center transition-all duration-500" :class="{ '!opacity-30 bounce': !isViewReached(view) }" />
+          </div>
         </div>
       </div>
-      <h1 class="text-4xl my-4">Quiz - test your knowledge</h1>
+
+    <div v-if="!gameStarted">
+      <h1 class="text-4xl ">Quiz - Test your knowledge</h1>
       <p class="leading-relaxed text-lg">
         Welcome to the quiz ! <br/>
         This quiz consist of
         <strong>{{ totalQuestions }}</strong>
         question. <br/>
         Please answer each question without using any outside help. <br/>
-        if you do not know the answer to a question, just go with your best
+        If you do not know the answer to a question, just go with your best
         guess.
       </p>
       <button
-          class="bg-gray-300 px-4 py-6 text-lg border-2 border-gray-800 rounded-md my-10 hover:bg-gray-400 ease-in-out duration-300"
+          class="gamifiedButton"
           @click="startGame"
       >
         Start the quiz
       </button>
     </div>
     <div v-if="gameStarted">
-      <div v-if="gameStarted && !showResult">
-        <h2 class="text-3xl my-4">
+      <div v-if="!showResult" class="">
+        <h2 class="text-3xl my-2">
           Question <strong>{{ currentQuestionIndex[currentView] + 1 }}</strong> of
           <strong>{{ questionsPrRound }}</strong>
         </h2>
@@ -42,11 +61,8 @@
             @answer="answerQuestion"
         />
       </div>
-
       <div v-if="showResult" class="space-y-4">
-
         <div v-for="(evaluation, category) in evaluations" :key="evaluation">
-
           <div v-if="evaluation.correctAnswers > 0">
             <p class="text-2xl" :class="'text-' + viewStyles[currentView].color + '-500'">
               {{ category }}: {{ evaluation.correctAnswers }} correct
@@ -55,8 +71,7 @@
           </div>
         </div>
         <p class="text-2xl">Thank you for playing !</p>
-        <button
-        class="bg-gray-400 px-4 py-6 text-lg border-2 border-gray-800 rounded-md my-10 hover:bg-gray-500 ease-in-out duration-300"
+        <button class="gamifiedButton"
             @click="resetGame"
         >
           Return to homepage
@@ -65,6 +80,7 @@
 
     </div>
   </div>
+  </div>
 </template>
 
 <script>
@@ -72,7 +88,7 @@ import QuestionAll from "@/components/declarativeKnowledge/QuestionAll.vue";
 import {computed, reactive, ref} from "vue";
 import PanumNavigation from "@/components/PanumNavigation.vue";
 import questions from "@/questions.json";
-import CorrectIcon from "@/components/declarativeKnowledge/icons/CorrectIcon.vue";
+import StarIcon from "@/components/declarativeKnowledge/icons/StarIcon.vue";
 
 export default {
   name: "DeclarativeKnowledge",
@@ -98,12 +114,13 @@ export default {
       'Feedback',
     ]
     const viewStyles =  {
-      'Welcome': { color: 'gray-500', image: CorrectIcon },
-      'Social Science': { color: 'red-500', image: CorrectIcon },
-      'Natural Science': { color: 'blue-500', image: CorrectIcon },
-      'Humanities': { color: 'yellow-500', image: CorrectIcon },
-      'Feedback': { color: 'gray-500', image: CorrectIcon },
+      'Welcome': { color: 'gray-500', image: StarIcon },
+      'Social Science': { color: 'red-500', image: StarIcon },
+      'Natural Science': { color: 'blue-500', image: StarIcon },
+      'Humanities': { color: 'yellow-500', image: StarIcon },
+      'Feedback': { color: 'gray-500', image: StarIcon },
     }
+
 
     const currentView = ref(views[0])
     const currentQuestionIndex = reactive({
@@ -117,8 +134,20 @@ export default {
       'Humanities': {correctAnswers: 0},
     });
 
+    const progress = computed(() => {
+      const viewIndex = views.indexOf(currentView.value);
+      const answeredQuestions = currentQuestionIndex[currentView.value] ? currentQuestionIndex[currentView.value] : 0;
+      const totalQuestions = questionsPrRound;
+      const totalProgress = Math.min((viewIndex * 25 ) + 3  + (answeredQuestions / totalQuestions * 20),100)
+      // Calculate progress as a percentage
+      return totalProgress;
+    });
 
-
+    const isViewReached = (view) => {
+      const currentViewIndex = views.indexOf(currentView.value);
+      const targetViewIndex = views.indexOf(view);
+      return currentViewIndex >= targetViewIndex;
+    };
     const changeView = (newView) => {
       currentView.value = newView;
       currentQuestionIndex['Social Science'] = 0;
@@ -127,6 +156,9 @@ export default {
     };
     const currentQuestion = computed(() => {
       const category = currentView.value;
+      if (!questions[category] || !questions[category][currentQuestionIndex[category]]) {
+        return null;
+      }
       return questions[category][currentQuestionIndex[category]];
     });
     const answerQuestion = (isCorrect) => {
@@ -147,13 +179,12 @@ export default {
         const nextViewIndex = views.indexOf(currentView.value) + 1;
         if (nextViewIndex < views.length) {
           changeView(views[nextViewIndex]);
-        } else {
-          // All questions are answered in all categories, show result
-          showResult.value = true;
+          if (currentView.value === 'Feedback') {
+            showResult.value = true;
+          }
         }
       }
     };
-
 
     const startGame = () => {
       currentView.value = 'Social Science';
@@ -165,7 +196,10 @@ export default {
       showResult.value = false;
       correctAnswer.value = 0;
       currentQuestionIndex.value = 0;
-      currentView.value = 'Social Science';
+      for (const key in evaluations) {
+        evaluations[key].correctAnswers = 0;
+      }
+      currentView.value = 'Welcome';
     };
 
     return {
@@ -187,7 +221,8 @@ export default {
       currentQuestion,
       viewStyles,
       views,
-      CorrectIcon
+      progress,
+      isViewReached
     };
   },
 };
--- src/components/declarativeKnowledge/QuestionAll.vue
+++ src/components/declarativeKnowledge/QuestionAll.vue
@@ -1,46 +1,46 @@
 <template>
   <div>
-    <div v-if="!showFeedback">
-      <p class="text-3xl my-4">
+<!--    <div v-if="!showFeedback">-->
+      <p class="text-3xl my-4 h-16 grid place-items-center">
         {{ question && question.text }}
       </p>
       <hr />
-      <div v-for="(option, index) in question && question.options" :key="index">
+      <div class="grid grid-cols-2 gap-6 my-6">
+      <div v-for="(option, index) in question && question.options" :key="index" >
         <button
-          class="w-60 h-14 bg-gray-300 hover:bg-gray-400 duration-300 ease-in-out active:bg-gray-900 border-2 border-gray-900 rounded-md my-2"
+          class="gamifiedButton w-full h-24"
           @click="checkOption(option)"
         >
           {{ option }}
         </button>
       </div>
-    </div>
-    <div v-if="showFeedback" class="flex flex-col items-center justify-center">
-      <div v-if="isCorrect">
-        <p class="text-2xl my-4 border-b border-b-green-500">Correct</p>
-        <CorrectIcon />
       </div>
-      <div v-else>
-        <p class="text-2xl my-4 border-b border-b-red-500">Incorrect</p>
-        <InCorrectIcon />
-      </div>
-    </div>
+<!--    </div>-->
+<!--    <div v-if="showFeedback" class="flex flex-col items-center justify-center">-->
+<!--      <div v-if="isCorrect">-->
+<!--        <p class="text-2xl my-4 border-b border-b-green-500">Correct</p>-->
+<!--        <CorrectIcon />-->
+<!--      </div>-->
+<!--      <div v-else>-->
+<!--        <p class="text-2xl my-4 border-b border-b-red-500">Incorrect</p>-->
+<!--        <InCorrectIcon />-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
-import CorrectIcon from "@/components/declarativeKnowledge/icons/CorrectIcon.vue";
-import InCorrectIcon from "@/components/declarativeKnowledge/icons/InCorrectIcon.vue";
+// import CorrectIcon from "@/components/declarativeKnowledge/icons/CorrectIcon.vue";
+// import InCorrectIcon from "@/components/declarativeKnowledge/icons/InCorrectIcon.vue";
 import { ref } from "vue";
 export default {
   components: {
-    CorrectIcon,
-    InCorrectIcon,
   },
   props: {
     question: Object,
   },
   setup(props, { emit }) {
-    const showFeedback = ref(false);
+    // const showFeedback = ref(false);
     const isCorrect = ref(false);
 
     const checkOption = (option) => {
@@ -49,14 +49,16 @@ export default {
       } else {
         isCorrect.value = false;
       }
-      showFeedback.value = true;
-      setTimeout(() => {
-        showFeedback.value = false;
         emit("answer", isCorrect.value);
-      }, 2000);
+      // showFeedback.value = true;
+      // setTimeout(() => {
+      //   showFeedback.value = false;
+      //   emit("answer", isCorrect.value);
+      // }, 2000);
     };
 
-    return { showFeedback, isCorrect, checkOption };
+    return { isCorrect, checkOption };
+    // return { showFeedback, isCorrect, checkOption };
   },
 };
 </script>
